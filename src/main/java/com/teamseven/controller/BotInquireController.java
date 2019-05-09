a112e631c90bad652a6667c138f4b105bca7c222
Unwana Essien <unwana.essien@outlook.com>
2019-05-10 00:50:45 +0100

    frontend commit

--- src/main/java/com/teamseven/controller/BotInquireController.java
+++
@@ -1,9 +0,0 @@
-package com.teamseven.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class BotInquireController {
-
-
-}
--- src/main/java/com/teamseven/service/BotInquireService.java
+++
@@ -1,5 +0,0 @@
-package com.teamseven.service;
-
-
-public interface BotInquireService {
-}
