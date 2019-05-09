560d0f8daacd3949b446cc17b12ffcdb51dcd18c
John Ojetunde <jojetunde@teamapt.com>
2019-05-09 23:44:51 +0100

    Initial commit of the Project

---
+++ src/main/java/com/teamseven/service/impl/DEBotInquireImpl.java
@@ -0,0 +1,9 @@
+package com.teamseven.service.impl;
+
+import com.teamseven.service.BotInquireService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DEBotInquireImpl implements BotInquireService {
+    
+}
