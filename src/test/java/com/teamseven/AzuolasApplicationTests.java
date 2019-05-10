bf770929ca7744a55dab4da5ff893af0798fb870
_kbluue_ <kbluue@yahoo.com>
2019-05-10 14:40:00 +0100

    sprint 3 - batch 2 - pull for implementation of db

--- src/test/java/com/teamseven/AzuolasApplicationTests.java
+++ src/test/java/com/teamseven/AzuolasApplicationTests.java
@@ -1,9 +1,18 @@
 package com.teamseven;
 
+import com.sun.org.apache.xpath.internal.compiler.Keywords;
+import com.teamseven.apimodel.AzuolasRequest;
+import com.teamseven.enums.KeywordsEnum;
+import com.teamseven.service.KeywordProvider;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +20,10 @@ public class AzuolasApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void extractKeywordsTest(){
 	}
 
 }
